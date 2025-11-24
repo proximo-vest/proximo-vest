7faea5c7040f2264d1f5af3ba569e580b35133ad
enrico-marques <enricothulio.marques@gmail.com>
2025-11-24 15:01:13 -0300

    commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,5 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    // opcional: logs para debug
-    // log: ["query", "error", "warn"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+export const prisma = new PrismaClient({ adapter })
